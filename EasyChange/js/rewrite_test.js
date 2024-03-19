8ecae5b33fd0879f381e6975de88ca3c17143608
Ilya Eskin <lego.rom.com@gmail.com>
2024-03-19 21:36:33 +0500

    added filters buttons and stupid changable filter content just for tests time

--- EasyChange/js/rewrite_test.js
+++ EasyChange/js/rewrite_test.js
@@ -1,6 +1,14 @@
 const inputImage = document.querySelector(".input-image");
 const chooseImgBtn = document.querySelector(".header__buttons__button-choose");
 const previewImg = document.querySelector(".wrapper__main-content__image");
+const filters = document.querySelector(".wrapper__sidebar-left__filter-variations__button-filters");
+const filtersContent = document.querySelector(".wrapper__sidebar-left__filter-content__filters-option");
+const cut = document.querySelector(".wrapper__sidebar-left__filter-variations__button-cut");
+const cutContent = document.querySelector(".wrapper__sidebar-left__filter-content__cut-option");
+const rotate = document.querySelector(".wrapper__sidebar-left__filter-variations__button-rotate");
+const rotateContent = document.querySelector(".wrapper__sidebar-left__filter-content__rotate-option");
+const text = document.querySelector(".wrapper__sidebar-left__filter-variations__button-text");
+const textContent = document.querySelector(".wrapper__sidebar-left__filter-content__text-option");
 
 const loadImage = () => {
     let image = inputImage.files[0];
@@ -11,8 +19,44 @@ const loadImage = () => {
         resetFilterBtn.click();
         document.querySelector(".container").classList.remove("disable");
     });
+}
+
+filters.addEventListener("click", () => {
+    filtersContent.removeAttribute("hidden");
+    cutContent.setAttribute("hidden", true);
+    rotateContent.setAttribute("hidden", true);
+    textContent.setAttribute("hidden", true);
 
 }
+);
+cut.addEventListener("click", () => {
+        cutContent.removeAttribute("hidden");
+        filtersContent.setAttribute("hidden", true);
+        rotateContent.setAttribute("hidden", true);
+        textContent.setAttribute("hidden", true);
+
+    }
+);
+rotate.addEventListener("click", () => {
+        rotateContent.removeAttribute("hidden");
+        filtersContent.setAttribute("hidden", true);
+        cutContent.setAttribute("hidden", true);
+        textContent.setAttribute("hidden", true);
+
+    }
+);
+text.addEventListener("click", () => {
+        textContent.removeAttribute("hidden");
+        filtersContent.setAttribute("hidden", true);
+        rotateContent.setAttribute("hidden", true);
+        cutContent.setAttribute("hidden", true);
+
+    }
+);
+
+
+
+
 inputImage.addEventListener("change", loadImage);
 chooseImgBtn.addEventListener("click", () => inputImage.click());
 
@@ -23,37 +67,4 @@ chooseImgBtn.addEventListener("click", () => inputImage.click());
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const fileInput = document.querySelector(".input-image");
-// const chooseImgBtn = document.querySelector(".header__buttons__button-choose");
-
-// const loadImage = () => {
-//     let file = fileInput.files[0];
-//     if(!file) return;
-//     previewImg.src = URL.createObjectURL(file);
-//     previewImg.addEventListener("load", () => {
-//         resetFilterBtn.click();
-//         document.querySelector(".container").classList.remove("disable");
-//     });
-// }
-
-
-
-
-// fileInput.addEventListener("change", loadImage);
-// chooseImgBtn.addEventListener("click", () => fileInput.click());
 
